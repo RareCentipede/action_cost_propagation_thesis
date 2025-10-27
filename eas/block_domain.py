961ac8de5f9088787f8de8e497201989410525bb
Zhengyang Lu <stevenlu176@gmail.com>
2025-10-27 12:27:33 +0100

    Need to figure out a way to connect all the variables

--- eas/block_domain.py
+++ eas/block_domain.py
@@ -1,3 +1,26 @@
 from enum import Enum
 from dataclasses import dataclass
+from typing import Tuple, List, Optional
+
+@dataclass
+class Pos:
+    name: str
+    pos: Tuple[float, float, float]
+    clear: bool = True
+    occupied_by: Optional['Object'] = None
+
+@dataclass
+class Object:
+    name: str
+    at: Pos
+    at_top: bool = True
+    on: Optional['Object'] = None
+    below: Optional['Object'] = None
+
+@dataclass
+class Robot:
+    name: str
+    at: Pos
+    holding: Optional[Object] = None
+    gripper_empty: bool = True
 
