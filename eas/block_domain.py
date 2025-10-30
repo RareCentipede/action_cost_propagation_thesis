614c24f78b6f2ef181a40170d626c87864eb791e
Zhengyang Lu <stevenlu176@gmail.com>
2025-10-30 18:47:50 +0100

    Built the graph

--- eas/block_domain.py
+++ eas/block_domain.py
@@ -2,15 +2,17 @@ import numpy as np
 
 from enum import Enum
 from dataclasses import dataclass, field
-from typing import Any, Tuple, List, Optional, NewType, Dict, Union, Callable, Type, ClassVar
+from typing import Any, Sequence, Tuple, List, Optional, NewType, Dict, Union, Callable, Type, ClassVar, cast
 from abc import abstractmethod
 
 Poses = NewType('Poses', Dict[str, 'Pose'])
 Objects = NewType('Objects', Dict[str, 'Object'])
 State = NewType('State', Dict[str, Any])
-Condition = Callable[..., bool]
+SimpleCondition = NewType('SimpleCondition', Tuple[str, str, Any])
+ComputedCondition = Callable[..., bool]
+Condition = Union[SimpleCondition, ComputedCondition]
 
-@dataclass
+@dataclass(eq=False)
 class Thing:
     name: str
     # variables describe which attributes compose the state for this Thing.
@@ -30,15 +32,26 @@ class Thing:
     def __str__(self):
         return self.state.__str__()
 
-@dataclass
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        if not isinstance(other, Thing):
+            return False
+        return self.name == other.name
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name})"
+
+@dataclass(eq=False)
 class NonePose(Thing):
     name: str = "NonePose"
 
-@dataclass
+@dataclass(eq=False)
 class NoneObj(Thing):
     name: str = "NoneObject"
 
-@dataclass
+@dataclass(eq=False)
 class Pose(Thing):
     pos: Tuple[float, float, float]
     orientation: Tuple[float, float, float, float] = (0.0, 0.0, 0.0, 1.0)
@@ -46,7 +59,7 @@ class Pose(Thing):
     occupied_by: 'Object | NoneObj' = field(default_factory=NoneObj)
     variables = ("clear", "occupied_by")
 
-@dataclass
+@dataclass(eq=False)
 class Object(Thing):
     at: Pose | NonePose
     at_top: bool = True
@@ -54,7 +67,7 @@ class Object(Thing):
     below: 'Object | NoneObj' = field(default_factory=NoneObj)
     variables = ("at", "at_top", "on", "below")
 
-@dataclass
+@dataclass(eq=False)
 class Robot(Thing):
     at: Pose
     holding: 'Object | NoneObj' = field(default_factory=NoneObj)
@@ -64,40 +77,56 @@ class Robot(Thing):
 move_parameters = {'robot': Robot,
                    'start_pose': Pose,
                    'target_pose': Pose}
-move_conditions = [lambda robot, start_pose, target_pose: robot.at == start_pose,
-                   lambda robot, start_pose, target_pose: robot.at != target_pose]
-move_effects = [lambda robot, start_pose, target_pose: setattr(robot, 'at', target_pose)]
+move_conditions = [SimpleCondition(('robot', 'at', 'start_pose')),
+                   SimpleCondition(('target_pose', 'occupied_by', '~robot'))]
+move_effects = [SimpleCondition(('robot', 'at', 'target_pose')),
+                SimpleCondition(('start_pose', 'occupied_by', '~robot'))]
 
 pick_parameters = {'robot': Robot,
                    'object': Object,
                    'object_pose': Pose}
-pick_conditions = [lambda robot, object, object_pose: robot.at == object_pose,
-                   lambda robot, object, object_pose: robot.gripper_empty,
-                   lambda robot, object, object_pose: object.at == object_pose,
-                   lambda robot, object, object_pose: object.at_top]
-pick_effects = [lambda robot, object, object_pose: setattr(robot, 'holding', object),
-                lambda robot, object, object_pose: setattr(robot, 'gripper_empty', False),
-                lambda robot, object, object_pose: setattr(object, 'at', None),
-                lambda robot, object, object_pose: setattr(object_pose, 'occupied_by', None)]
+pick_conditions = [SimpleCondition(('robot', 'at', 'object_pose')),
+                   SimpleCondition(('robot', 'gripper_empty', True)),
+                   SimpleCondition(('object', 'at', 'object_pose')),
+                   SimpleCondition(('object', 'at_top', True))]
+pick_effects = [SimpleCondition(('robot', 'holding', 'object')),
+                SimpleCondition(('robot', 'gripper_empty', False)),
+                SimpleCondition(('object', 'at', NonePose())),
+                SimpleCondition(('object_pose', 'occupied_by', NoneObj()))]
 
 place_parameters = {'robot': Robot,
                     'object': Object,
                     'target_pose': Pose}
-place_conditions = [lambda robot, object, target_pose: robot.at == target_pose,
-                    lambda robot, object, target_pose: robot.holding == object,
-                    lambda robot, object, target_pose: target_pose.clear]
-place_effects = [lambda robot, object, target_pose: setattr(robot, 'holding', None),
-                 lambda robot, object, target_pose: setattr(robot, 'gripper_empty', True),
-                 lambda robot, object, target_pose: setattr(object, 'at', target_pose),
-                 lambda robot, object, target_pose: setattr(target_pose, 'occupied_by', object),
-                 lambda robot, object, target_pose: setattr(target_pose, 'clear', False)]
+place_conditions = [SimpleCondition(('robot', 'at', 'target_pose')),
+                    SimpleCondition(('robot', 'holding', 'object')),
+                    SimpleCondition(('target_pose', 'clear', True))]
+place_effects = [SimpleCondition(('robot', 'holding', NoneObj())),
+                 SimpleCondition(('robot', 'gripper_empty', True)),
+                 SimpleCondition(('object', 'at', 'target_pose')),
+                 SimpleCondition(('target_pose', 'occupied_by', 'object')),
+                 SimpleCondition(('target_pose', 'clear', False))]
 
-def apply_action(parameters: Dict[str, Thing], conditions: List[Condition], effects: List[Callable]) -> bool:
-    if not all(cond(*parameters.values()) for cond in conditions):
-        return False
+def apply_action(parameters: Dict[str, Thing], conditions: List[SimpleCondition], effects: List[SimpleCondition]) -> bool:
+    for cond in conditions:
+        if isinstance(cond, Tuple):
+            parent_name, attr, value = cond
+            parent = parameters[parent_name]
+            if isinstance(value, str):
+                value = parameters[value]
+
+            if not (getattr(parent, attr) == value):
+                if not (isinstance(value, str) and value.startswith('~')):
+                    return False
 
     for effect in effects:
-        effect(*parameters.values())
+        if isinstance(effect, Tuple):
+            parent_name, attr, value = effect
+            parent = parameters[parent_name]
+
+            if isinstance(value, str):
+                value = parameters[value]
+
+            setattr(parent, attr, value)
 
     return True
 
@@ -105,7 +134,9 @@ def apply_action(parameters: Dict[str, Thing], conditions: List[Condition], effe
 class Domain:
     things: Dict[Type[Thing], List[Thing]]
     states: List[State] = field(default_factory=list)
-    actions: Dict[str, Tuple[Dict[str, Thing], List[Condition], List[Callable]]] = field(default_factory=dict)
+    actions: Dict[str, Tuple[Dict[str, Thing],
+                             Sequence[SimpleCondition | ComputedCondition],
+                             Sequence[SimpleCondition | ComputedCondition]]] = field(default_factory=dict)
 
     @property
     def current_state(self) -> State:
@@ -132,7 +163,7 @@ init_state = State({})
 for thing_list in things_init.values():
     for thing in thing_list:
         init_state.update(thing.state)
-
+    
 domain = Domain(things=things_init, states=[init_state], actions={'move': (move_parameters, move_conditions, move_effects)})
 
 # Build Domain Transition Graphs (DTGs)
@@ -141,15 +172,17 @@ dtg = {}
 @dataclass
 class Node:
     name: str
+    type: str
     values: Tuple[Any, ...]
     applicable_actions: List[str] = field(default_factory=list)
-    edges: List[Tuple[str, Any]] = field(default_factory=list)
+    edges: List[Tuple[str, 'Node']] = field(default_factory=list)
 
     def __str__(self):
         node_name = f"Node: {self.name}, "
-        values = f"values: {[value.name if hasattr(value, 'name') else value for value in self.values]}, "
+        values = f"values: {[f'{value.name}: {type(value).__name__}' if hasattr(value, 'name') else value for value in self.values]}, "
+        applicable_actions = f"applicable_actions: {self.applicable_actions}, "
         edges = f"edges: {[(edge[0], edge[1].name if hasattr(edge[1], 'name') else edge[1]) for edge in self.edges]}"
-        return node_name + values + edges
+        return node_name + values + applicable_actions + edges
 
 # Different approach:
 # First get all the variables (predicates)
@@ -157,57 +190,108 @@ class Node:
 # Finally, connect nodes based on possible actions
 
 # TODO: Need to take care of the cases where block is on/below itself
-for thing_type in domain.things.values():
-    for thing in thing_type:
+for thing_type, things in domain.things.items():
+    for thing in things:
         for attribute in thing.variables:
+            if attribute != 'at' and attribute != 'holding':
+                continue
+
+            vars = []
             var_type = type(getattr(thing, attribute))
 
             if var_type is NonePose:
                 var_type = Pose
+                vars = [cast(Thing, NonePose())]
             elif var_type is NoneObj:
                 var_type = Object
+                vars = [cast(Thing, NoneObj())]
+            elif var_type is Pose:
+                vars = [cast(Thing, NonePose())]
+            elif var_type is Object:
+                vars = [cast(Thing, NoneObj())]
 
-            vars = domain.things.get(var_type, [])
+            if var_type is bool:
+                vars = [True, False]
+            else:
+                vars.extend(domain.things.get(var_type, []))
 
             for var in vars:
                 var_name = getattr(var, 'name', var)
                 node_name = f"{thing.name}_{attribute}_{var_name}"
-                dtg[node_name] = Node(name=node_name, values=(thing, var))
+                dtg[node_name] = Node(name=node_name, type=attribute, values=(thing, var))
                 # Maybe add applicable actions to the nodes here based on the values
+
+                if type(thing) is Robot and attribute == 'at':
+                    # Move action only applies to robot's 'at' attribute
+                    dtg[node_name].applicable_actions.append('move')
+
+                else:
+                    dtg[node_name].applicable_actions = ['pick', 'place']
 
                 # print(dtg[node_name])
 
 # Get only nodes involving robot, move actions are only for robots
 robot_nodes = [node for node in dtg.values() if type(node.values[0]) is Robot]
 
-# for node in robot_nodes:
-#     value_types = [type(v) for v in node.values]
-#     if value_types not in list(move_parameters.values()):
-#             continue
-#     other_nodes = [n for n in robot_nodes if n != node]
-#     for other in other_nodes:
-#         value_types = [type(v) for v in node.values]
-#         if value_types != list(move_parameters.values()):
-#             continue
-
-#         edge = ('move', other)
-#         if edge not in node.edges:
-#             node.edges.append(edge)
-
-#     print(node)
-
 new_dtg = {}
 
 # Get all values for corresponding parameters in move action
-move_param_vals = {}
-nodes = dtg.values()
-relevant_nodes = []
-for param_name, param in move_parameters.items():
-    move_param_vals[param_name] = domain.things.get(param, [])
+# move edges
+move_nodes = [node for node in dtg.values() if 'move' in node.applicable_actions]
+for node in move_nodes:
+    other_nodes = [n for n in move_nodes if n != node]
+    for other in other_nodes:
+        edge = ('move', other)
 
-    for node in nodes:
-        if param.name in node.values:
-            relevant_nodes.append(node)
-            print(node)
+        if edge not in node.edges:
+            node.edges.append(edge)
 
-    nodes = relevant_nodes
+pick_place_nodes = [node for node in dtg.values() if 'move' not in node.applicable_actions]
+node_types = set(node.type for node in pick_place_nodes)
+# for node in pick_place_nodes:
+#     node_type = node.type
+#     other_nodes = [n for n in pick_place_nodes if n != node]
+
+#     match node_type:
+#         case 'holding':
+#             other_nodes = [n for n in other_nodes if n.type == 'holding']
+#             node.edges = [('pick', other) for other in other_nodes]
+
+#             for other in other_nodes:
+#                 other.edges.append(('place', node))
+
+    # print(node)
+print(node_types)
+for node_type in node_types:
+    selected_nodes = [node for node in pick_place_nodes if node.type == node_type]
+
+    match node_type:
+        case 'at':
+            none_nodes = [n for n in selected_nodes if n.values[1] in (NoneObj(), NonePose())]
+            other_nodes = [n for n in selected_nodes if n not in none_nodes and n.values[1] not in (NoneObj(), NonePose())]
+
+            for none_node in none_nodes:
+                same_obj_nodes = [n for n in other_nodes if n.values[0].name == none_node.values[0].name]
+                none_node.edges = [('place', other) for other in same_obj_nodes]
+
+                for other in same_obj_nodes:
+                    other.edges.append(('pick', none_node))
+
+        case 'gripper_empty':
+            true_node = [n for n in selected_nodes if n.values[1] is True][0]
+            false_node = [n for n in selected_nodes if n.values[1] is False][0]
+
+            true_node.edges = [('pick', false_node)]
+            false_node.edges = [('place', true_node)]
+
+        case 'holding':
+            none_node = [n for n in selected_nodes if n.values[1] in (NoneObj(), NonePose())][0]
+            other_nodes = [n for n in selected_nodes if n != none_node]
+
+            none_node.edges = [('pick', other) for other in other_nodes]
+            for other in other_nodes:
+                other.edges.append(('place', none_node))
+
+
+for node in dtg.values():
+    print(node)
