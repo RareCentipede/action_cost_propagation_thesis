860bee5a2792684fb92722e99c268ce31f0f5644
Zhengyang Lu <stevenlu176@gmail.com>
2025-11-19 13:48:04 +0100

    Test

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-import numpy as np
-a = np.array([1, 2, 3, 4, 5])
+from eas.block_domain import Pose, Robot, Object, Ground
 
-b = np.array([a, a])
+p1 = Pose("p1", (0.0, 0.0, 0.0))
+p2 = Pose("p2", (1.0, 0.0, 0.0))
+p3 = Pose("p3", (2.0, 0.0, 0.0))
+b1 = Object("b1", p1, False, Ground())
+b2 = Object("b2", p2, True, b1)
+b1.below = b2
 
-print(b.size)
+setattr(b2, 'on.at_top', True)
+print(b1.at_top)
